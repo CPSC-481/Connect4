64171933fd64b1f98a5f0a93a6c2acd7d7bd23ca
daniel <danielsollis2@gmail.com>
2018-10-18 11:47:52 -0700

    small changes

--- connectFour.py
+++ connectFour.py
@@ -5,7 +5,12 @@ from PyQt5.QtWidgets import QMainWindow,  \
                             QPushButton, \
                             QLabel
 import sys
+from enum import Enum
 
+class Colors(Enum):
+    white = 0
+    red = 1
+    blue = 2
 
 class Qt_window(QMainWindow):
     def __init__(self):
@@ -16,6 +21,7 @@ class Qt_window(QMainWindow):
         self.setCentralWidget(self.mainWindow)
         self.mainWindow.setLayout(self.grid)
         self.show()
+        self.matrix = self.initGameMatrix()
 
     def initGrid(self):
         for i in range(0, 7):
@@ -26,8 +32,14 @@ class Qt_window(QMainWindow):
         for i in range(7, self.grid.count()):
             child = self.grid.itemAt(i).widget()
             child.setStyleSheet("background-color: white; border-radius: 10; border: 1px inset black; min-height: 40")
-
-
+    def initGameMatrix
+        matrix = []
+        for i in range(0, 6):
+            newColumn = []
+            for j in range(0, 5):
+                newColumn.append(Colors.white)
+            matrix.append(newColumn)
+        return matrix
 if __name__ == "__main__":
     q_app = QApplication(sys.argv)
     q_window = Qt_window()
